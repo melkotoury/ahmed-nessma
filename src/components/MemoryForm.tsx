9c906988ff31c3c12480e4973f8b8fa9e10b2eda
Mahmoud El Kotoury <melkotoury@gmail.com>
2024-07-02 19:14:55 +0300

    fix material form attr

--- src/components/MemoryForm.tsx
+++ src/components/MemoryForm.tsx
@@ -16,16 +16,38 @@ export function MemoryForm() {
         color='transparent'
         className='border-2 border-purple-600 m-2 p-10 border-r-4'
         shadow={false}
+        placeholder={undefined}
+        onPointerEnterCapture={undefined}
+        onPointerLeaveCapture={undefined}
       >
-        <Typography variant='h4' color='blue-gray'>
+        <Typography
+          variant='h4'
+          color='blue-gray'
+          placeholder={undefined}
+          onPointerEnterCapture={undefined}
+          onPointerLeaveCapture={undefined}
+        >
           Share your memory
         </Typography>
-        <Typography color='gray' className='mt-1 font-normal'>
+        <Typography
+          color='gray'
+          className='mt-1 font-normal'
+          placeholder={undefined}
+          onPointerEnterCapture={undefined}
+          onPointerLeaveCapture={undefined}
+        >
           We are happy to see you sharing a memory with us
         </Typography>
         <form className='mt-8 mb-2 w-80 max-w-screen-lg sm:w-96'>
           <div className='mb-1 flex flex-col gap-6'>
-            <Typography variant='h6' color='blue-gray' className='-mb-3'>
+            <Typography
+              variant='h6'
+              color='blue-gray'
+              className='-mb-3'
+              placeholder={undefined}
+              onPointerEnterCapture={undefined}
+              onPointerLeaveCapture={undefined}
+            >
               Your Name
             </Typography>
             <Input
@@ -36,8 +58,18 @@ export function MemoryForm() {
               labelProps={{
                 className: 'before:content-none after:content-none',
               }}
+              onPointerEnterCapture={undefined}
+              onPointerLeaveCapture={undefined}
+              crossOrigin={undefined}
             />
-            <Typography variant='h6' color='blue-gray' className='-mb-3'>
+            <Typography
+              variant='h6'
+              color='blue-gray'
+              className='-mb-3'
+              placeholder={undefined}
+              onPointerEnterCapture={undefined}
+              onPointerLeaveCapture={undefined}
+            >
               Your Message
             </Typography>
             <Textarea
@@ -49,11 +81,26 @@ export function MemoryForm() {
                 className: 'before:content-none after:content-none',
               }}
               rows={8}
+              onPointerEnterCapture={undefined}
+              onPointerLeaveCapture={undefined}
             />
-            <Typography variant='h6' color='blue-gray' className='-mb-3'>
+            <Typography
+              variant='h6'
+              color='blue-gray'
+              className='-mb-3'
+              placeholder={undefined}
+              onPointerEnterCapture={undefined}
+              onPointerLeaveCapture={undefined}
+            >
               Upload file
             </Typography>
-            <Button className='mt-6 p-6 bg-purple-700 text-white' fullWidth>
+            <Button
+              className='mt-6 p-6 bg-purple-700 text-white'
+              fullWidth
+              placeholder={undefined}
+              onPointerEnterCapture={undefined}
+              onPointerLeaveCapture={undefined}
+            >
               Share a Memory
             </Button>
           </div>
