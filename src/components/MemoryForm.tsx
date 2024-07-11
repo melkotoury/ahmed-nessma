817bb6b81ca4cdb645fa1c5ea13883891e62f786
Mahmoud El Kotoury <melkotoury@gmail.com>
2024-07-11 16:38:16 +0300

    first commit

--- src/components/MemoryForm.tsx
+++ src/components/MemoryForm.tsx
@@ -49,9 +49,7 @@ export function MemoryForm() {
 
         <form
           className='mt-8 mb-2 w-80 max-w-screen-lg sm:w-96'
-          method='POST'
           name='memories'
-          netlify
         >
           <input type='hidden' name='form-name' value='memories' />
           <div className='mb-1 flex flex-col gap-6'>
